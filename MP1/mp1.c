0a27617eefd7bb8cbe4b09690b5bee23a568f0f7
Jesse Zhang <macisasandwich@gmail.com>
2016-02-13 15:18:30 -0600

    set up the Linux linked list

--- MP1/mp1.c
+++ MP1/mp1.c
@@ -2,6 +2,7 @@
 
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/list.h>
 #include "mp1_given.h"
 
 MODULE_LICENSE("GPL");
@@ -9,6 +10,28 @@ MODULE_AUTHOR("Group_17");
 MODULE_DESCRIPTION("CS-423 MP1");
 
 #define DEBUG 1
+
+struct process {
+   int pid;
+   unsigned long cpu_use;
+   struct list_head list;
+};
+
+struct process processList;
+
+static ssize_t mp1_read (struct file *file, char user *buffer, size_t count, loff_t*data){
+   // implementation goes here...
+}
+
+static ssize_t mp1_write (struct file *file, const char user *buffer, size_t count, loff_t*data){
+   // implementation goes here...
+}
+
+static const struct file_operations mp1_file = {
+   .owner = THIS_MODULE,
+   .read = mp1_read,
+   .write = mp1_write,
+};
 
 // mp1_init - Called when module is loaded
 int __init mp1_init(void)
@@ -33,9 +56,22 @@ void __exit mp1_exit(void)
    #endif
    // Insert your code here ...
    printk(KERN_INFO "See ya.\n");
+   list_cleanup();
    
 
    printk(KERN_ALERT "MP1 MODULE UNLOADED\n");
+}
+
+// this function safely deletes and frees the linked list
+void list_cleanup() {
+   struct process *aProcess, *tmp;
+
+   printk(KERN_INFO "Cleaning up processList\n");
+   list_for_each_entry_safe(aProcess, tmp, &processList.list, list) {
+      printk(KERN_INFO "MP1 freeing PID %d\n", aProcess->pid);
+      list_del(&aProcess->list);
+      kfree(aProcess);
+   }
 }
 
 // Register init and exit funtions
