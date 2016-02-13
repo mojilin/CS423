48e31ed2b651f189d89c2fbb9f5dc89767b777c0
Jesse Zhang <macisasandwich@gmail.com>
2016-02-13 16:26:20 -0600

    fixed null dereference bug in list_cleanup

--- MP1/mp1.c
+++ MP1/mp1.c
@@ -38,7 +38,7 @@ struct process {
    struct list_head list;
 };
 
-struct process processList;
+struct list_head processList;
 
 void list_cleanup(void);
 
@@ -60,7 +60,7 @@ static const struct file_operations mp1_file = {
 void list_cleanup(void) {
    struct process *aProcess, *tmp;
 
-   if (list_empty(&processList.list) == 0) {
+   if (list_empty(&processList) == 0) {
       printk(KERN_INFO "Cleaning up processList\n");
       list_for_each_entry_safe(aProcess, tmp, &processList.list, list) {
          #ifdef DEBUG
@@ -81,6 +81,8 @@ int __init mp1_init(void)
    #endif
    // Insert your code here ...
    printk(KERN_INFO "Hello World!\n");
+
+   INIT_LIST_HEAD(processList);
    
    proc_dir = proc_mkdir(DIRECTORY, NULL);
    proc_entry = proc_create(FILENAME, 0666, proc_dir, &mp1_file);  //create entry in proc system
