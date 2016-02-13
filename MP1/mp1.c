9bc2a12f1dd58a7b4d590c87028b4daa98e0c87c
Caleb <perknsn2@illinois.edu>
2016-02-13 15:21:49 -0600

    beginning of proc

--- MP1/mp1.c
+++ MP1/mp1.c
@@ -9,6 +9,25 @@ MODULE_AUTHOR("Group_ID");
 MODULE_DESCRIPTION("CS-423 MP1");
 
 #define DEBUG 1
+#define FILENAME "status"
+#define DIRECTORY "mp1"
+
+//structs for proc filesystem
+static struct proc_dir_entry *proc_dir;
+static struct proc_dir_entry *proc_entry;
+
+static ssize_t mp1_read(struct file *file, char__user *buffer, size_t count, loff_t *data){
+   //TODO
+}
+static ssize_t mp1_write(struct file *file, const char__user *buffer, size_t count, loff_t *data){
+
+}
+
+static const struct file_operations mp1_file = {
+   .owner = THIS_MODULE,
+   .read = mp1_read,
+   .write = mp1_write,
+};
 
 // mp1_init - Called when module is loaded
 int __init mp1_init(void)
@@ -18,7 +37,8 @@ int __init mp1_init(void)
    #endif
    // Insert your code here ...
    
-   
+   proc_dir = proc_mkdir(DIRECTORY, NULL);
+   proc_entry = proc_create(FILENAME, 0666, proc_dir, &mp1_file);  //create entry in proc system
    
    printk(KERN_ALERT "MP1 MODULE LOADED\n");
    return 0;   
@@ -32,7 +52,8 @@ void __exit mp1_exit(void)
    #endif
    // Insert your code here ...
    
-   
+   remove_proc_entry(FILENAME, proc_dir);
+   proc_remove(proc_dir);
 
    printk(KERN_ALERT "MP1 MODULE UNLOADED\n");
 }
