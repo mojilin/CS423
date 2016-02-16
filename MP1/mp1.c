46825b902d320ef7b36a9141dd641a91ebd0553e
ACJV <you@example.com>
2016-02-16 00:01:21 -0600

    Wrote basic mp1_read, needs testing

--- MP1/mp1.c
+++ MP1/mp1.c
@@ -16,6 +16,9 @@ MODULE_DESCRIPTION("CS-423 MP1");
 #define FILENAME "status"
 #define DIRECTORY "mp1"
 
+#define min(a,b) (((a) < (b)) ? (a) : (b))
+#define max(a,b) (((a) > (b)) ? (a) : (b))
+
 //structs for proc filesystem
 static struct proc_dir_entry *proc_dir;
 static struct proc_dir_entry *proc_entry;
@@ -30,18 +33,55 @@ struct list_head processList;
 
 void list_cleanup(void);
 
-static ssize_t mp1_read (struct file *file, char __user *buffer, size_t count, loff_t *data) {
-   return 0;
+static ssize_t mp1_read (struct file *file, char __user *buffer, size_t count, loff_t *data) 
+{
+   char * tempBuffer = kmalloc(count, GFP_KERNEL);
+   struct process *newProcess, *tmp;
+   int writeCount = 0;
+   int temp;
+   if(tempBuffer == NULL)
+   {
+       printk(KERN_WARN "malloc failed");
+       return 0;
+   }  
+   
+   /* Iterate thorugh the list and print the list data into the string */
+   /******************************* LOOOOOOOOCK THISSSS *******************/
+   list_for_each_entry_safe(thisProcess, tmp, &processList, list) {
+		
+	writeCount += snprintf(tempBuffer+writeCount,(count- writeCount) , "PID %d: %d", tmp->pid, tmp->cpu_use); 
+
+	/* If done, Null terminate and return */
+	if(writeCount >= count) {	
+	   tempBuffer[count - 1] = '\0';
+	   break;
+	}
+	/* Else turn the NULL into a newline to continue the string */
+	else {
+		tempBuffer[writeCount - 1] = '\n';
+	}	
+		
+   }
+
+   /* Copy the string to user and return the right value */
+   
+   temp = copy_to_user(tempBuffer, buffer, min(count, writeCount));
+
+   kfree(tempBuffer);
+   return temp;
 }
 
-static ssize_t mp1_write (struct file *file, const char __user *buffer, size_t count, loff_t *data) {
+static ssize_t mp1_write (struct file *file, const char __user *buffer, size_t count, loff_t *data) 
+{
    struct process *newProcess, *tmp, *thisProcess;
    newProcess = (process *)kmalloc(sizeof(struct process), GFP_KERNEL);
 
+   // VIB: CHECK IF MALLOC FAILED
+  
    newProcess->pid = newPID;     //register new PID
 
    
-   if (list_empty(&processList) == 0){
+   if (list_empty(&processList) == 1){ // VIB: Changed from 0 to 1
       INIT_LIST_HEAD(newProcess->list);
       list_add(newProcess, processList);     //add to new to head
 
@@ -59,6 +99,7 @@ static ssize_t mp1_write (struct file *file, const char __user *buffer, size_t c
          }
       }
    }
+   // FREEEEEEE MEMORY
    return 0;
 }
 
@@ -69,7 +110,8 @@ static const struct file_operations mp1_file = {
 };
 
 // this function safely deletes and frees the linked list
-void list_cleanup(void) {
+void list_cleanup(void) 
+{
    struct process *aProcess, *tmp;
 
    if (list_empty(&processList) == 0) {
