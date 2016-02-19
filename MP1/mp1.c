c5f2d054be75b69d1d86d6cc7ececfb4d2830a9e
ACJV <you@example.com>
2016-02-18 22:58:57 -0600

    reimplemented write and cleaned up code

--- MP1/mp1.c
+++ MP1/mp1.c
@@ -27,49 +27,56 @@ static struct proc_dir_entry *proc_dir;
 static struct proc_dir_entry *proc_entry;
 static struct time_list work_timer;
 
-spinlock_t list_lock ;//= SPIN_LOCK_UNLOCKED;
+spinlock_t *list_lock;
 
-typedef struct process {
+typedef struct process 
+{
    unsigned int pid;
    unsigned long cpu_use;
    struct list_head list;
-}process;
+} process;
 
 struct list_head processList;
 
+/* Function prototypes */
 void list_cleanup(void);
+int add_process (int pid);
 
 
 static ssize_t mp1_read (struct file *file, char __user *buffer, size_t count, loff_t *data) 
 {
    char * tempBuffer = kmalloc(count, GFP_KERNEL);
-   process *newProcess, *tmp, *thisProcess;
+   process *thisProcess;
    int writeCount = 0;
    int temp;
    if(tempBuffer == NULL)
    {
-       printk(KERN_WARNING "malloc failed");
+       printk(KERN_WARNING "read malloc failed");
        return 0;
    }  
    
    /* Iterate thorugh the list and print the list data into the string */
-   /******************************* LOOOOOOOOCK THISSSS *******************/
-   list_for_each_entry_safe(thisProcess, tmp, &processList, list) {
+   spin_lock(list_lock); 
+   list_for_each_entry(thisProcess, &processList, list) 
+   {
 		
-	writeCount += snprintf(tempBuffer+writeCount,(count- writeCount) , "PID %u: %lu", tmp->pid, tmp->cpu_use); 
+		writeCount += snprintf(tempBuffer+writeCount,(count- writeCount),
+					   	"PID %u: %lu", thisProcess->pid, thisProcess->cpu_use); 
 
-	/* If done, Null terminate and return */
-	if(writeCount >= count) {	
-	   tempBuffer[count - 1] = '\0';
-	   break;
-	}
-	/* Else turn the NULL into a newline to continue the string */
-	else {
-		tempBuffer[writeCount - 1] = '\n';
-	}	
-		
+		/* If done, Null terminate and return */
+		if(writeCount >= count) 
+		{	
+			tempBuffer[count - 1] = '\0';
+			break;
+		}	
+		/* Else turn the NULL into a newline to continue the string */
+		else 
+		{
+			tempBuffer[writeCount - 1] = '\n';
+		}	
+				
    }
-
+   spin_unlock(list_lock);
    /* Copy the string to user and return the right value */
    
    temp = copy_to_user(buffer, tempBuffer, min((int)count, writeCount));
@@ -78,54 +85,61 @@ static ssize_t mp1_read (struct file *file, char __user *buffer, size_t count, l
    return temp;
 }
 
-static ssize_t mp1_write (struct file *file, const char __user *buffer, size_t count, loff_t *data) 
+int add_process (int pid)
 {
-   process *newProcess, *tmp, *thisProcess;
-   char tempBuffer[count];
-   int newPID;
-   newProcess = kmalloc(sizeof(process), GFP_KERNEL);
+	process * newProcess = kmalloc(sizeof(process), GFP_KERNEL);
+	if(newProcess == NULL)
+	{
+		printk(KERN_WARNING "add malloc failed");
+		return 0;
+	}
 
-   if (newProcess == NULL){
-      printk(KERN_WARNING "malloc failed");
-      return -1;
-   }
+	newProcess->pid = pid;
+	newProcess->cpu_use = 0;
 
-   copy_from_user(tempBuffer,buffer,count);
-   kstrtol(tempBuffer, 10,(long int *)&newProcess->pid);     //register new PID
-   newProcess->cpu_use = 0;
-   
-   /* check if the list is empty and just append the new process if it is*/
-   if (list_empty(&processList) != 0){
-      INIT_LIST_HEAD(&newProcess->list);
+	/* Add to the linked list */
+	INIT_LIST_HEAD(&newProcess->list);
 
-      spin_lock(&list_lock);
-      list_add(&newProcess->list, &processList);     //add to new to head
-      spin_unlock(&list_lock);
 
-      //TODO   add reading PID from user
-   }
-   /* If the list has something in it...*/
-   else {
-      list_for_each_entry_safe(thisProcess, tmp, &processList, list){
-         //Check if this process has already been inserted
-         if(thisProcess->pid == newPID){
-            kfree(newProcess);
-            return -1; 
-         }
-         //if not, add it in
-         else if(tmp == &processList){
-            INIT_LIST_HEAD(&newProcess->list);
-            
-            spin_lock(&list_lock);
-            list_add(&newProcess->list, &thisProcess->list);
-            spin_unlock(&list_lock);
-         }
-      }
-   }
-   return count;
+	spin_lock(list_lock);
+	list_add( &newProcess->list, &processList);
+	spin_unlock(list_lock);
+
+	return 1;
+
 }
 
-static const struct file_operations mp1_file = {
+static ssize_t mp1_write (struct file *file, const char __user *buffer, size_t count, loff_t *data) 
+{
+	char * tempBuffer = kmalloc(count, GFP_KERNEL);
+	long int temp;	
+	if(tempBuffer == NULL)
+	{
+		printk(KERN_WARNING "write malloc failed");
+		return 0;
+	}
+
+	temp = copy_from_user(tempBuffer, buffer, count);
+	if(temp == 0)
+		goto write_fail;
+
+	/* Convert str to int */
+	if(kstrtol(tempBuffer, 10, &temp) != 0)
+		goto write_fail;
+
+	temp = add_process(temp);
+
+	kfree(tempBuffer);
+
+	return (temp == 0) ? 0 : count;
+write_fail:
+	printk(KERN_WARNING "write copy failed");
+	kfree(tempBuffer);
+	return 0;	
+}
+
+static const struct file_operations mp1_file = 
+{
    .owner = THIS_MODULE,
    .read = mp1_read,
    .write = mp1_write,
@@ -136,9 +150,12 @@ void list_cleanup(void)
 {
    process *aProcess, *tmp;
 
-   if (list_empty(&processList) == 0) {
+   spin_lock(list_lock);
+   if (list_empty(&processList) == 0) 
+   {
       printk(KERN_INFO "Cleaning up processList\n");
-      list_for_each_entry_safe(aProcess, tmp, &processList, list) {
+      list_for_each_entry_safe(aProcess, tmp, &processList, list) 
+      {
          #ifdef DEBUG
          printk(KERN_INFO "MP1 freeing PID %d\n", aProcess->pid);
          #endif
@@ -147,6 +164,8 @@ void list_cleanup(void)
          kfree(aProcess);
       }
    }
+
+   spin_unlock(list_lock);
 }
 
 // mp1_init - Called when module is loaded
@@ -162,6 +181,14 @@ int __init mp1_init(void)
    // work_timer.function = work_time_handler;
    // work_timer.expires = jiffies + 5*HZ;
    // work_timer.data = 0;
+
+   list_lock = kmalloc(sizeof(spinlock_t), GFP_KERNEL);
+   if(list_lock == NULL)
+   {
+		printk(KERN_WARNING "spinlock malloc failed");
+		return -1;
+   }
+   spin_lock_init(list_lock);
 
    INIT_LIST_HEAD(&processList);
    
@@ -186,6 +213,7 @@ void __exit mp1_exit(void)
 
    remove_proc_entry(FILENAME, proc_dir);
    proc_remove(proc_dir);
+   kfree(list_lock);
 
    printk(KERN_ALERT "MP1 MODULE UNLOADED\n");
 }
