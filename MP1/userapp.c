bb1a1eee48e2775e342e277820fb99ded2bb9334
ACJV <you@example.com>
2016-02-20 04:03:50 -0600

    added fclose

--- MP1/userapp.c
+++ MP1/userapp.c
@@ -9,13 +9,11 @@ int main(int argc, char* argv[])
 {
 	FILE* handle = fopen("/proc/mp1/status","r+");
 	int thePid = (int) getpid();
-	char str[15];
 	fprintf(handle, "%d", thePid);
 
-	//memset(str, 0, 15);
 	printf("Current PID: %d\n", thePid);
-
-	//printf("Read String:\n %s\n", str);
+	
+	fclose(handle);
 
 	return 0;
 }
