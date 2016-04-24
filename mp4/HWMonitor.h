36c4403c3224787eac5dffcbb13fe141d64ac15b
Jesse Zhang <macisasandwich@gmail.com>
2016-04-24 18:24:05 -0500

    HWMonitor stuff

--- mp4/HWMonitor.h
+++ mp4/HWMonitor.h
@@ -1,21 +1,11 @@
 #ifndef HWM_H
 #define HWM_H
 
-#include <linux/pid.h>
-#include <linux/sched.h>
-#include <mutex>
+#include <pthread.h>
 
-#define find_task_by_pid(nr) pid_task(find_vpid(nr), PIDTYPE_PID)
+double get_cpu_use();
+void set_throttle(double new_throttle);
+double get_throttle();
 
-class HWMonitor {
-	public:
-		int get_cpu_use(int pid, unsigned long *cpu_use);
-		double get_throttle();
-		void set_throttle(double new_throttle);
-
-	private:
-		std::mutex throttle_lock;
-		double throttle;
-};
 
 #endif
