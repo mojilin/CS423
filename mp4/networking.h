45d0d4a0866028ee7809172d596fe3a1606401e5
Potato <starch@potato.com>
2016-04-25 13:53:23 -0500

    moar networking

--- mp4/networking.h
+++ mp4/networking.h
@@ -21,8 +21,9 @@ typedef enum {JOB_TRANSFER} n_state;
 
 int join_channel(const char * address, int port);
 int create_channel(int port);
-int channel_write(int fd, void * buf, int nbytes);
 
+int channel_write(int fd, n_state state,void * buf, int nbytes);
+int channel_read(int fd, void * buf, int nbytes);
 
 void error(const char *msg);
 
