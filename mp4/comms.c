8806eb8955100e116336b4469a9458eef1082ba8
ACJV <you@example.com>
2016-04-25 20:21:55 -0500

    comms QOL

--- mp4/comms.c
+++ mp4/comms.c
@@ -5,12 +5,19 @@
 
 char buffer[100];
 
+int i =0 ;
 void transfer_job(int fd, Job_t job)
 {
-	printf("Transferring job %d. Data: %f\n", job.id, job.data);
-
-	sprintf(buffer, "id: %d, data: %lf", job.id, job.data);
-	channel_write(fd, JOB_TRANSFER, buffer, 100);
+	char temp[100];
+	printf("Transferring job %d. Data: %f\n", job.id + i, job.data + i);
+	sprintf(buffer, "%d %lf", job.id + i, (job.data + i));
+	i++;
+	
+	write(fd, buffer, 100);
+	while(read(fd, temp, 100) <= 0);
+	printf("Message Acknoledged: %s\n", temp);
+	read(fd,temp,100);
+	
 }
 
 int main()
@@ -19,13 +26,14 @@ int main()
 	job.id = 10;
 	job.data = 2.345;
 	
+	int j;
 	int sockfd = create_channel(PORT);
-	while(1)
+	for( j =0 ; j < 100; j++)
 	{
-		getc(stdin);
 		transfer_job(sockfd, job);
 	}
 
+	while(1);
 	close(sockfd);
 	return 0;
 }
